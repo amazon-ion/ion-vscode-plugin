b89d78ea134f9739e150cbbad7d9872bb96edaff
Ziqi Li <lziq@amazon.com>
2020-08-20 12:42:17 -0500

    Made modification according to the previous PR comment.

--- client/src/ui-test/.mocharc-debug.js
+++ client/src/ui-test/.mocharc-debug.js
@@ -1,3 +1,5 @@
+// This file is the global setting of default timeout for each test case. 
+// Using per-test case timeout will override the global setting. 
 module.exports = {
     timeout: 99999999
 }
--- client/src/ui-test/parenthesis-test.ts
+++ client/src/ui-test/parenthesis-test.ts
@@ -1,14 +1,15 @@
 import * as assert from 'assert'; 
-import { TextEditor, EditorView, StatusBar, InputBox, ContentAssist, Workbench } from "vscode-extension-tester";
+import { TextEditor, EditorView, StatusBar, InputBox, Workbench } from "vscode-extension-tester";
 
 describe('auto-closing', () => {
     let editor: TextEditor;
     let view: EditorView;
+    let DEFAULT_TIMEOUT : number = 5000; 
 
     before(async function() {
-        this.timeout(8000);
+        this.timeout (DEFAULT_TIMEOUT);
         await new Workbench().executeCommand('File: New File');
-        await new Promise((res) => { setTimeout(res, 1000); });
+        await sleep (1000); 
         view = new EditorView();
         editor = new TextEditor(view);
 
@@ -17,7 +18,7 @@ describe('auto-closing', () => {
         await input.setText('ion');
         await input.confirm();
 
-        await editor.setText ('\n\n\n\n\n\n\n\n\n\n'); 
+        await editor.setText ('\n\n\n\n\n\n\n\n\n\n\n\n\n\n'); 
     });
 
     after(async () => {
@@ -26,7 +27,7 @@ describe('auto-closing', () => {
     });
 
     it('rounded brackets', async function() {
-        this.timeout(5000);
+        this.timeout(DEFAULT_TIMEOUT);
         await editor.typeText (1, 1, '(');
 
         const line = await editor.getTextAtLine(1);
@@ -34,7 +35,7 @@ describe('auto-closing', () => {
     });
 
     it('square brackets', async function() {
-        this.timeout(5000);
+        this.timeout(DEFAULT_TIMEOUT);
         await editor.typeText (2, 1, '[');
 
         const line = await editor.getTextAtLine(2);
@@ -42,15 +43,15 @@ describe('auto-closing', () => {
     });
 
     it('curly brackets', async function() {
-        this.timeout(5000);
-        await editor.typeText (3, 1, '{}');
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (3, 1, '{');
 
         const line = await editor.getTextAtLine(3);
         assert.equal (line, '{}');
     });
 
     it('brackets combination', async function() {
-        this.timeout(5000);
+        this.timeout(DEFAULT_TIMEOUT);
         await editor.typeText (4, 1, '{');
         await editor.typeText (4, 2, '[');
         await editor.typeText (4, 3, '(');
@@ -60,54 +61,103 @@ describe('auto-closing', () => {
     });
 
     it('single quote', async function() {
-        this.timeout(5000);
+        this.timeout(DEFAULT_TIMEOUT);
         await editor.typeText (5, 1, '\'');
 
         const line = await editor.getTextAtLine(5);
         assert.equal (line, '\'\'');
     });
 
-    it('double quote', async function() {
-        this.timeout(5000);
+    it('double quotes', async function() {
+        this.timeout(DEFAULT_TIMEOUT);
         await editor.typeText (6, 1, '\"');
 
         const line = await editor.getTextAtLine(6);
         assert.equal (line, '\"\"');
     });
 
-    it('single quote in double quote', async function() {
-        // The auto-closing should not work in this case. 
-        this.timeout(5000);
-        await editor.typeText (7, 1, '\"\'');
+    it('triple quotes', async function() {
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (7, 1, '\'\'\'');
 
         const line = await editor.getTextAtLine(7);
+        assert.equal (line, '\'\'\'\'\'\'');
+    });
+
+    it('single quote in double quotes', async function() {
+        // Auto-closing should not work in string. 
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (8, 1, '\"\'');
+
+        const line = await editor.getTextAtLine(8);
         assert.equal (line, '\"\'\"');
     });
 
-    it('block comment', async function() {
-        // The auto-closing should not work in this case. 
-        this.timeout(5000);
-        await editor.typeText (8, 1, '/*');
+    it('single quote in triple quotes', async function() {
+        // Auto-closing should not work in string. 
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (9, 1, '\'\'\'');
+        await editor.typeText (9, 4, '  ');
+        await editor.typeText (9, 5, '\'');
 
-        const line = await editor.getTextAtLine(8);
+        const line = await editor.getTextAtLine(9);
+        assert.equal (line, '\'\'\' \' \'\'\'');
+    });
+
+    it('double quotes in triple quotes', async function() {
+        // Auto-closing should not work in string. 
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (10, 1, '\'\'\'\"');
+
+        const line = await editor.getTextAtLine(10);
+        assert.equal (line, '\'\'\'\"\'\'\'');
+    });
+
+    it('triple quotes in double quotes', async function() {
+        // Auto-closing should not work in string. 
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (11, 1, '\"\'\'\'');
+
+        const line = await editor.getTextAtLine(11);
+        assert.equal (line, '\"\'\'\'\"');
+    });
+
+    it('block comment', async function() {
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (12, 1, '/*');
+
+        const line = await editor.getTextAtLine(12);
         assert.equal (line, '/* */');
     });
 
     it('quotes in line comment', async function() {
-        // The auto-closing should not work in this case. 
-        this.timeout(5000);
-        await editor.typeText (9, 1, '//\'\"');
+        // Auto-closing should not work in comment. 
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (13, 1, '//\'\"\'\'\'');
 
-        const line = await editor.getTextAtLine(9);
-        assert.equal (line, '//\'\"');
+        const line = await editor.getTextAtLine(13);
+        assert.equal (line, '//\'\"\'\'\'');
     });
 
     it('quotes in block comment', async function() {
-        // The auto-closing should not work in this case. 
-        this.timeout(5000);
-        await editor.typeText (10, 1, '/*\'\"');
+        // Auto-closing should not work in comment. 
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (14, 1, '/*\'\"\'\'\'');
 
-        const line = await editor.getTextAtLine(10);
-        assert.equal (line, '/*\'\" */');
+        const line = await editor.getTextAtLine(14);
+        assert.equal (line, '/*\'\"\'\'\' */');
+    });
+
+    it('blobs and clobs', async function() {
+        this.timeout(DEFAULT_TIMEOUT);
+        await editor.typeText (15, 1, '{{');
+        await editor.typeText (15, 3, ' 1111 ');
+
+        const line = await editor.getTextAtLine(15);
+        assert.equal (line, '{{ 1111 }}');
     });
 });
+
+async function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
