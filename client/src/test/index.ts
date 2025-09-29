a9b380b03dbad477fe4b97129003280a978f9c9f
Joshua Barr <jobarr@amazon.com>
2025-09-29 15:51:04 -0700

    Add an explicit dependency on glob

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -12,28 +12,24 @@ export function run(): Promise<void> {
 
 	const testsRoot = __dirname;
 
-	return new Promise((resolve, reject) => {
-		glob('**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return reject(err);
-			}
+	return new Promise(async (resolve, reject) => {
+		try {
+			const files = await glob('**.test.js', { cwd: testsRoot });
 
 			// Add files to the test suite
 			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						reject(new Error(`${failures} tests failed.`));
-					} else {
-						resolve();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				reject(err);
-			}
-		});
+			// Run the mocha test
+			mocha.run(failures => {
+				if (failures > 0) {
+					reject(new Error(`${failures} tests failed.`));
+				} else {
+					resolve();
+				}
+			});
+		} catch (err) {
+			console.error(err);
+			reject(err);
+		}
 	});
 }
