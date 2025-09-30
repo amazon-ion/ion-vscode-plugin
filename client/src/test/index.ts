1c8c50e7cdca924ad0d2c7ba505aec9fd7f0de0d
Joshua Barr <jobarr@amazon.com>
2025-09-30 08:26:06 -0700

    Don't try to chain Promise

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -12,26 +12,24 @@ export function run(): Promise<void> {
 
 	const testsRoot = __dirname;
 
-	return new Promise((resolve, reject) => {
-		glob('**.test.js', { cwd: testsRoot }).then((files) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const files = await glob('**.test.js', { cwd: testsRoot });
+
 			// Add files to the test suite
 			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						reject(new Error(`${failures} tests failed.`));
-					} else {
-						resolve();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				reject(err);
-			}
-		}, (err) => {
+			// Run the mocha test
+			mocha.run(failures => {
+				if (failures > 0) {
+					reject(new Error(`${failures} tests failed.`));
+				} else {
+					resolve();
+				}
+			});
+		} catch (err) {
+			console.error(err);
 			reject(err);
-		});
+		}
 	});
 }
